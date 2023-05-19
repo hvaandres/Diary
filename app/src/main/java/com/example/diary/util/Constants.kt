b71c21137fc2901059f143b1a57b0e4aa2ebd260
hvaandres <hvaandres@icloud.com>
2023-05-19 16:38:11 -0600

    Creating app with gcloud for the authentication with mongodb and adding the appid and clientid into my android project

---
+++ app/src/main/java/com/example/diary/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.diary.util
+
+object Constants {
+
+    const val APP_ID = "application-0-szplk"
+    const val CLIENT_ID = "998135740494-in7mck8v80erra2rc8vggaob6145q50v.apps.googleusercontent.com"
+}
