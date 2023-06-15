2b0d471d068715cb27535d86272fa01d674048d5
hvaandres <hvaandres@icloud.com>
2023-06-15 16:01:42 -0600

    Adding Autnetication content and screen

---
+++ app/src/main/java/com/example/diary/presentation/screens/auth/AuthenticationContent.kt
@@ -0,0 +1,9 @@
+package com.example.diary.presentation.screens.auth
+
+import androidx.compose.runtime.Composable
+import com.example.diary.navigation.Screen
+
+@Composable
+fun Screen.AuthenticationContent(){
+
+}
---
+++ app/src/main/java/com/example/diary/presentation/screens/auth/AuthenticationScreen.kt
@@ -0,0 +1,30 @@
+package com.example.diary.presentation.screens.auth
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.stevdzasan.diaryapp.util.Constants.CLIENT_ID
+import com.stevdzasan.messagebar.ContentWithMessageBar
+import com.stevdzasan.messagebar.MessageBarState
+import com.stevdzasan.onetap.OneTapSignInState
+import com.stevdzasan.onetap.OneTapSignInWithGoogle
+
+@ExperimentalMaterial3Api
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun AuthenticationScreen(){
+    Scaffold(
+        content = {
+            AuthenticationContent()
+        }
+    )
+}
